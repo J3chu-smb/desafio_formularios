b09988d6a15cc2c7c46f095483b3f193e088e2ec
María Jesús Fuenzalida Palma <jechufuenzalida10@gmail.com>
2019-11-11 18:06:01 -0300

    my first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'sales/new'
+  post 'sales/create', as: 'sales'
+  get 'sales/done'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
